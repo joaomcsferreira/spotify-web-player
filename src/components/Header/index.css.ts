75e71e72f50fb0d64939c733b9aaf1ba32230ddb
joaomcsferreira <marcosc024@gmail.com>
2023-08-25 17:16:19 -0300

    refactor: Moved header component styling to a separate file

--- src/components/Header/index.css.ts
+++ src/components/Header/index.css.ts
@@ -1,3 +1,13 @@
-const styles = {}
+const styles = {
+  container: "h-16 p-4 flex justify-between items-center",
+  nav: "flex gap-2 items-center",
+  navItem: "p-2 bg-black/60 rounded-full cursor-pointer",
+  search: "p-3 h-12 min-w-[25rem] flex items-center gap-3 bg-nero rounded-full",
+  searchIcon: "w-4 brightness-50",
+  searchInput:
+    "bg-transparent border-none pt-1 font-light text-sm outline-none",
+  profile: "p-1 bg-black/70 rounded-full cursor-pointer",
+  avatar: "w-6 h-6 bg-indigo-600 rounded-full",
+}
 
 export default styles
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,18 +9,7 @@ import { usePathname } from "next/navigation"
 import backIcon from "../../../public/svg/less-than.svg"
 import nextIcon from "../../../public/svg/greater-than.svg"
 import searchIcon from "../../../public/svg/search.svg"
-
-const styles = {
-  container: "h-16 p-4 flex justify-between items-center",
-  nav: "flex gap-2 items-center",
-  navItem: "p-2 bg-black/60 rounded-full cursor-pointer",
-  search: "p-3 h-12 min-w-[25rem] flex items-center gap-3 bg-nero rounded-full",
-  searchIcon: "w-4 brightness-50",
-  searchInput:
-    "bg-transparent border-none pt-1 font-light text-sm outline-none",
-  profile: "p-1 bg-black/70 rounded-full cursor-pointer",
-  avatar: "w-6 h-6 bg-indigo-600 rounded-full",
-}
+import styles from "./index.css"
 
 const Header = () => {
   const pathname = usePathname()
